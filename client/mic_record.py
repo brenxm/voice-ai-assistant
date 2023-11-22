f6b2c781a04b6a14bdc1689b8861a58d5834a579
brymina18 <brymina18@yahoo.com>
2023-11-21 19:45:25 -0600

    initial commit

---
+++ client/mic_record.py
@@ -0,0 +1,9 @@
+
+# Recording voice prompt
+# Press 'f20'
+# Establish connection with server
+# While pressing, record voice data ~
+# Per cut off 1. Timeout - silent, 2. button up, create object with this properties
+# Metadata (obj) - size of payload (binary size)
+# Payload - audio data converted to binary
+# Send data
